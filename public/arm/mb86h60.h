6ac14cf348cb2eea261cc3e4fd20a423188c0dee
Spitzbube <spitzbube@mail.bg>
2025-06-01 20:38:03 +0200

    armle: mb86h60: add eth

--- public/arm/mb86h60.h
+++ public/arm/mb86h60.h
@@ -107,12 +107,15 @@
 #define MB86H60_VIC_ADDRESS         0xf00
 #define MB86H60_VIC_SIZE            0x1000
 
+#define MB86H60_ETH_BASE    0xe4000000
+#define MB86H60_ETH_SIZE            0x2000
+
 /* Never forget to update MB86H60_IRQ_CTRL_IRQMASK in init_intrinfo() */
 #define MB86H60_INTR_TIMER0     5
 #define MB86H60_INTR_UART0      8
 #define MB86H60_INTR_UART1      9
 #define MB86H60_INTR_DMA        20
 #define MB86H60_INTR_USB        21
-
+#define MB86H60_INTR_ETH        22
 
 #endif /* MB86H60_H_ */
